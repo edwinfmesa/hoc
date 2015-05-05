338c94190e540edc7a59d08a77737a6ff5ef7e26
edwinfmesa <edwinfmesa@hotmail.com>
2015-05-05 11:06:55 -0500

    Some bugs fixed

--- lexer.py
+++ lexer.py
@@ -27,22 +27,22 @@ constants = {
 }
 
 bltin = {
-    'sin':'SIN',
-    'cos':'COS',
-    'tan':'TAN',
-    'asin':'ASIN',
-    'acos':'ACOS',
-    'atan':'ATAN',
-    'sinh':'SINH',
-    'cosh':'COSH',
-    'tanh':'TANH',
-    'int':'INT',
-    'log':'LOG',
-    'log10':'LOG10',
-    'sqrt':'SQRT',
-    'abs':'ABS',
-    'erf':'ERF',
-    'erfc':'ERFC',
+    'sin':'BLTIN',
+    'cos':'BLTIN',
+    'tan':'BLTIN',
+    'asin':'BLTIN',
+    'acos':'BLTIN',
+    'atan':'BLTIN',
+    'sinh':'BLTIN',
+    'cosh':'BLTIN',
+    'tanh':'BLTIN',
+    'int':'BLTIN',
+    'log':'BLTIN',
+    'log10':'BLTIN',
+    'sqrt':'BLTIN',
+    'abs':'BLTIN',
+    'erf':'BLTIN',
+    'erfc':'BLTIN',
 
 }
 
@@ -83,7 +83,8 @@ tokens = [
    'LBRACKET',
    'RBRACKET',
    'COMMA',
-   'SEMICOLON'
+   'SEMICOLON',
+   'NEWLINE'
 ] 
 
 tokens += keywords.values() + constants.values() + bltin.values()
@@ -119,6 +120,7 @@ t_LBRACKET = r'{'
 t_RBRACKET = r'}'
 t_COMMA = r','
 t_SEMICOLON = r';'
+t_NEWLINE = r'\n'
 
 
 
@@ -152,7 +154,7 @@ def t_ID(t):
     elif  t.value in constants: 
       t.type=constants[t.value]
     elif t.value in bltin:
-      t.type=bltin[t.value]
+      t.type='BLTIN'
     return t
 
 def t_error_STRING(t):
@@ -166,114 +168,114 @@ def t_COMMENT(t):
 
 t_ignore = '\t '
 
-lexico = lex.lex()
+# lexico = lex.lex()
 
-t1="""
-3+4%
-1&&3
-2 ||3
-1!==3<
-4>1
-2<=2
-9>=10
-1++(3)
-1$2
-"""
+# t1="""
+# 3+4%
+# 1&&3
+# 2 ||3
+# 1!==3<
+# 4>1
+# 2<=2
+# 9>=10
+# 1++(3)
+# 1$2
+# """
 
-t2="""
+# t2="""
 
-if
-2+2
-i=1
-if
-pi
-PI
-sin 
-cos(0)
-5.0
-3E
-"""
+# if
+# 2+2
+# i=1
+# if
+# pi
+# PI
+# sin 
+# cos(0)
+# 5.0
+# 3E
+# """
 
-t3="""
-4.5
-3
-565.999
--34
--344.44
-4E
-5E1
-5E6
-6E-5
--3E-4
-"""
+# t3="""
+# 4.5
+# 3
+# 565.999
+# -34
+# -344.44
+# 4E
+# 5E1
+# 5E6
+# 6E-5
+# -3E-4
+# """
 
-t4="""
-3+4-8+2/4*8
-4+1+2*PHI
-print (7-8-1)+E
-tan(PI/3)+cos(2*pi)
-"""
+# t4="""
+# 3+4-8+2/4*8
+# 4+1+2*PHI
+# print (7-8-1)+E
+# tan(PI/3)+cos(2*pi)
+# """
 
-t5="""
-while (read(x)) {
-     print "Prueba de modulo ", x%40000
-} 
-## esto es un comentario en hoc
-hola
-"esto es un error 
-@
-"""
+# t5="""
+# while (read(x)) {
+#      print "Prueba de modulo ", x%40000
+# } 
+# ## esto es un comentario en hoc
+# hola
+# "esto es un error 
+# @
+# """
 
-t6='''
-proc squares(){ 
-    local i, j, k 
-    for (i=1; i <= $1; i=i+1){ 
-        print i*i 
-    }
-} 
-'''
+# t6='''
+# proc squares(){ 
+#     local i, j, k 
+#     for (i=1; i <= $1; i=i+1){ 
+#         print i*i 
+#     }
+# } 
+# '''
 
-t7="""
-func reciduous(){
-    if ($1 ==$2) {
-       return 1
-    } else {
-      return $1/$2
-    } 
-} 
-"""
-t8="""
-func cuadrado(){
-    if ($1 ==0) {
-       return 1
-    } else {
-      return $1*$1
-    } 
-} 
-"""
-t9="""
-func factorial(){
-    if ($1 ==0) {
-       return 1
-    } else {
-        while ($1 >0){
-            $2 += $1
-        }
-        $1= $1-1
-        return $2
-    } 
-} 
-"""
-t10="""
-func igual(){
-    if ($1 ==$2) {
-        return 1
-    } else {
-        return 0
-    } 
-} 
-igual(3,3)
-"""
+# t7="""
+# func reciduous(){
+#     if ($1 ==$2) {
+#        return 1
+#     } else {
+#       return $1/$2
+#     } 
+# } 
+# """
+# t8="""
+# func cuadrado(){
+#     if ($1 ==0) {
+#        return 1
+#     } else {
+#       return $1*$1
+#     } 
+# } 
+# """
+# t9="""
+# func factorial(){
+#     if ($1 ==0) {
+#        return 1
+#     } else {
+#         while ($1 >0){
+#             $2 += $1
+#         }
+#         $1= $1-1
+#         return $2
+#     } 
+# } 
+# """
+# t10="""
+# func igual(){
+#     if ($1 ==$2) {
+#         return 1
+#     } else {
+#         return 0
+#     } 
+# } 
+# igual(3,3)
+# """
 
 # test = t1
 # print test
@@ -288,7 +290,34 @@ igual(3,3)
 #    print tokenizer
 
     
+
+if __name__ == '__main__':
+    # Build the lexer
+    import sys 
     
+    lex = lex.lex()
+    
+    if len(sys.argv) > 1:
+        f = open(sys.argv[1],"r")
+        data = f.read()
+        f.close()
+    else:
+        data = ""
+        while 1:
+            try:
+                data += raw_input() + "\n"
+            except:
+                break
+    
+    lex.input(data)
+    
+    # Tokenize
+    while 1:
+            tok = lex.token()
+            if not tok: break            # No more input
+            print tok
+    
+      
     
     
     
--- parser.py
+++ parser.py
@@ -4,230 +4,307 @@ from lexer import *
 # -------------------------------------------------------
 
 def p_list_empty(p):
-    "list : "
+    "list : empty"
+    pass
 
 def p_list_nl(p):
-    " list : list \n "
+    " list : list NEWLINE "
+    pass
 
 def p_list_defn(p):
-    "list : list defn \n"
+    "list : list defn NEWLINE"
+    pass
 
 def p_list_asgn(p):
-    " list : list asgn \n "
+    " list : list asgn NEWLINE "
+    pass
 
 def p_list_stmt(p):
-    "list : list stmt \n"
+    "list : list stmt NEWLINE"
+    pass
 
 def p_list_expr(p):
-    "list : list expr \n"
+    "list : list expr NEWLINE"
+    pass
 
 def p_list_error(p):
-    "list : list error \n"
+    "list : list error NEWLINE"
+    pass
 
 # ----------------------------------------------------
 
 def p_asgn_equal(p):
     "asgn : ID ASSIGN expr "
+    pass
 
 def p_asgn_addeq(p):
     "asgn : ID ADDEQ expr "
+    pass
 
 def p_asgn_subeq(p):
     "asgn : ID SUBEQ expr "
+    pass
 
 def p_asgn_muleq(p):
     "asgn : ID MULEQ expr "
+    pass
 
 def p_asgn_diveq(p):
     "asgn : ID DIVEQ expr "
+    pass
 
 def p_asgn_modeq(p):
     "asgn : ID MODEQ expr "
+    pass
 
 # ------------------------------------------------------
 
 def p_stmt_expr(p):
     "stmt : expr"
+    pass
 
 def p_stmt_return(p):
     "stmt : RETURN"
+    pass
 
 def p_stmt_return_expr(p):
     "stmt : RETURN expr"
+    pass
 
 def p_stmt_proc(p):
-    "stmt : PROCEDURE begin LPARENT arglist RPARENT"
+    "stmt : PROCEDURE BEGIN LPARENT arglist RPARENT"
+    pass
 
 def p_stmt_print(p):
     "stmt : PRINT prlist"
+    pass
 
 def p_stmt_while(p):
     "stmt : WHILE  LPARENT cond RPARENT stmt END "
+    pass
 
 def p_stmt_for(p):
     "stmt : FOR LPARENT cond SEMICOLON cond SEMICOLON cond RPARENT stmt END"
+    pass
 
 def p_stmt_if(p):
     "stmt : IF LPARENT cond RPARENT stmt END"
+    pass
 
 def p_stmt_if_else(p):
     "stmt : IF LPARENT cond RPARENT stmt END ELSE stmt END"
+    pass
 
 def p_stmt_braket(p):
     "stmt : LBRACKET stmtlist RBRACKET "
+    pass
 
 # -----------------------------------------------------
 
 def p_cond_expr(p):
     "cond : expr "
+    pass
 
 # -----------------------------------------------------
 
 def p_stmtlist_empty(p):
-    "stmtlist : "
+    "stmtlist : empty "
+    pass
 
 def p_stmlist_newline(p):
-    "stmtlist : stmtlist \n"
+    "stmtlist : stmtlist NEWLINE"
+    pass
 
 def p_stmtlist_stmt(p):
     "stmtlist : stmtlist stmt "
+    pass
 
 # --------------------------------------------------------
 
 def p_expr_number(p):
     "expr : NUMBER"
+    pass
 
 def p_expr_id(p):
     "expr : ID "
+    pass
 
 def p_expr_asgn(p):
     "expr : asgn"
+    pass
 
 def p_expr_function(p):
     "expr : FUNCTION BEGIN"
+    pass
 
 def p_expr_read(p):
     "expr : LPARENT ID RPARENT"
+    pass
 
 def p_expr_bltin(p):
     "expr : ID LPARENT expr RPARENT"
+    pass
 
 def p_expr_parent(p):
     "expr : LPARENT expr RPARENT"
+    pass
 
 def p_expr_plus(p):
     'expr : expr PLUS expr'
+    pass
 
 def p_expr_minus(p):
     'expr : expr MINUS expr'
+    pass
     
 def p_expr_times(p):
     "expr : expr TIMES expr"
+    pass
 
 def p_expr_divide(p):
     "expr : expr DIVIDE expr"
+    pass
 
 def p_expr_mod(p):
     "expr : expr MOD expr"
+    pass
 
 def p_expr_munit(p):
     "expr : MINUS expr "
+    pass
 
 def p_expr_lt(p):
     'expr : expr LT expr'
+    pass
 
 def p_expr_gt(p):
     'expr : expr GT expr'
+    pass
 
 def p_expr_le(p):
     'expr : expr LE expr'
+    pass
 
 def p_expr_ge(p):
     'expr : expr GE expr'
+    pass
 
 def p_expr_eq(p):
     'expr : expr EQ expr'
+    pass
 
 def p_expr_ne(p):
     'expr : expr NE expr'
+    pass
     
 def p_expr_or(p):
     'expr : expr OR expr'
+    pass
 
 def p_expr_and(p):
     'expr : expr AND expr'
+    pass
 
 def p_expr_not(p):
     'expr : expr NOT expr'
+    pass
 
 def p_expr_inc(p):
     "expr : INC ID"
+    pass
 
 def p_expr_dec(p):
     "expr : DEC ID"
+    pass
 
 def p_expr_inc2(p):
     "expr : ID INC"
+    pass
 
 def p_expr_dec2(p):
     "expr : ID DEC"
+    pass
 
 # ------------------------------------------------------
 
 def p_prlist_expr(p):
     "prlist : expr"
+    pass
 
 def p_prlist_str(p):
     "prlist : STRING"
+    pass
 
 def p_prlist_comma_expr(p):
     "prlist : prlist COMMA expr"
+    pass
 
 def p_prlist_comma_str(p):
     "prlist : prlist COMMA STRING"
+    pass
 
 # ---------------------------------------------------------
 
 def p_defn_func(p):
     "defn : FUNC procname LPARENT formals RPARENT"
+    pass
 
 def p_defn_proc(p):
     "defn : PROC procname LPARENT formals RPARENT"
+    pass
 
 # -----------------------------------------------------------
 
 def p_formals_var(p):
     "formals : ID"
+    pass
 
 def p_formals_comma_var(p):
     "formals : ID COMMA formals"
+    pass
 
 # ------------------------------------------------------------
 
 def p_procname_function(p):
     "procname : FUNCTION"
+    pass
 
 def p_procname_id(p):
     "procname : ID"
+    pass
 
 def p_procname_proc(p):
     "procname : PROCEDURE"
+    pass
+
+def p_procname_bltin(p):
+    "procname : BLTIN"
 
 # ------------------------------------------------
 
 def p_arglist_empty(p):
-    "arglist : "
+    "arglist : empty"
+    pass
 
 def p_arglist_expr(p):
     "arglist : expr"
+    pass
 
 def p_arglist_comma_expr(p):
     "arglist : arglist COMMA expr"
+    pass
+
+# ----------------------------------------------------
+
+def p_empty(p):
+    "empty : "
+    pass
 
 # ---------------------------------------------------------
 
 def p_error(p):
     print "Syntax error in input!"
+    # return "ERROR"
 
 # -------------------------------------------------------
 
